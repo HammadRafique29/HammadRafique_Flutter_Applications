c30803d79f699ab162a58efac6eb34fdf63b127b
Hammad Rafique <codingmagician0@gmail.com>
2024-01-12 10:04:21 +0500

    New Feature 5
    Upload Post Data to Firebase

--- Crypto Trading Application/lib/screens/dashboard.dart
+++ Crypto Trading Application/lib/screens/dashboard.dart
@@ -7,7 +7,14 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:convert';
 import 'localDataStorage.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'dart:io';
+import 'package:trading_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path/path.dart' as p;
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:file_picker/file_picker.dart';
 
 int _currentIndex = 0;
 
@@ -458,22 +465,138 @@ class PageFour extends StatefulWidget {
 }
 
 class _PageFourState extends State<PageFour> {
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomRight,
-      child: Padding(
-        padding: EdgeInsets.only(right: 16.0, bottom: 16.0),
-        child: FloatingActionButton(
-          backgroundColor: Colors.amber[400]!.withOpacity(0.8),
-          onPressed: () {
-            // Add your onPressed logic here
-            showDialog(
-              context: context,
-              builder: (context) => PostDialog(),
+  List<Widget> Posts = [];
+  List<Widget> Images = [];
+  List<Widget> Videos = [];
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: "dQw4w9WgXcQ",
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: false,
+    ),
+  );
+
+  void getPosts() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+    final snapshot = await ref.child('Posts').get();
+    if (snapshot.exists) {
+      var data = snapshot.value as Map<dynamic, dynamic>;
+      for (var dataPost in data.entries) {
+        var dataPostItems = dataPost.value[0];
+
+        // print("### ${dataPostItems}");
+
+        for (int i = 0; i < dataPostItems["images"].length; i++) {
+          print(dataPostItems["images"][i]);
+          Images.add(Center(
+              child: Image.network(
+            dataPostItems["images"][i],
+            width: MediaQuery.sizeOf(context).width * 0.7,
+            height: 150,
+          )));
+          Images.add(SizedBox(height: 5));
+        }
+
+        String? videoId =
+            YoutubePlayer.convertUrlToId('${dataPostItems["video"][0]}');
+
+        if (videoId != null) {
+          _controller = YoutubePlayerController(
+            initialVideoId: videoId,
+            flags: YoutubePlayerFlags(
+              autoPlay: true,
+              mute: false,
+            ),
+          );
+        }
+
+        print("## ${dataPostItems}");
+        setState(
+          () {
+            Posts.add(
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 2, color: Colors.amber),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Center(
+                      child: Text("${dataPostItems["title"]}"),
+                    ),
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: EdgeInsets.only(left: 40, right: 40),
+                      child: Text("${dataPostItems["para"]}"),
+                    ),
+                    SizedBox(height: 5),
+                    Column(
+                      children: Images,
+                    ),
+                    SizedBox(height: 10),
+                    Center(
+                      child: Container(
+                        width: MediaQuery.sizeOf(context).width * 0.7,
+                        child: YoutubePlayer(
+                          controller: _controller,
+                          showVideoProgressIndicator: true,
+                          progressIndicatorColor: Colors.blueAccent,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
             );
           },
-          child: Icon(Icons.add, color: Colors.white),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: Posts,
+        ),
+      ),
+      floatingActionButton: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: EdgeInsets.only(right: 16.0, bottom: 16.0),
+          child: FloatingActionButton(
+            backgroundColor: Colors.amber[400]!.withOpacity(0.8),
+            onPressed: () {
+              // Add your onPressed logic here
+              showDialog(
+                context: context,
+                builder: (context) => PostDialog(),
+              );
+            },
+            child: Icon(Icons.add, color: Colors.white),
+          ),
         ),
       ),
     );
@@ -508,54 +631,61 @@ class coinPage extends StatelessWidget {
   }
 }
 
-// Define a custom dialog class that extends StatelessWidget
-class PostDialog extends StatelessWidget {
-  // Declare the text editing controllers for the title and the paragraph
+class PostDialog extends StatefulWidget {
+  const PostDialog({super.key});
+
+  @override
+  State<PostDialog> createState() => _PostDialogState();
+}
+
+class _PostDialogState extends State<PostDialog> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController paragraphController = TextEditingController();
+  List<dynamic> files = [];
 
-// Define a function to pick an image from the gallery and return it as a File
   Future<File?> _pickImage() async {
-    // Use the image_picker package to get the image
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.gallery);
-
-    // Return the image as a File if it is not null, otherwise return null
     return pickedFile != null ? File(pickedFile.path) : null;
   }
 
   Future<File?> _pickVideo() async {
-    // Use the image_picker package to get the video
     final pickedFile =
         await ImagePicker().pickVideo(source: ImageSource.gallery);
-
-    // Return the video as a File if it is not null, otherwise return null
     return pickedFile != null ? File(pickedFile.path) : null;
   }
 
-  Future<void> _uploadVideo(File? video, String name) async {
-    // Check if the video is not null
-    if (video != null) {
-      // Create a reference to the firebase storage
-      FirebaseStorage storage = FirebaseStorage.instance;
+  // Future<void> uploadFile(File file) async {
+  //   await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  //   );
+  //   firebase_storage.FirebaseStorage storage =
+  //       firebase_storage.FirebaseStorage.instance;
+  //   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  //   firebase_storage.Reference ref =
+  //       storage.ref().child("post_images/"); //p.basename(file.path));
 
-      // Create a reference to the specific location with the given name
-      Reference ref = storage.ref().child(name);
+  //   // Upload the file to firebase
+  //   firebase_storage.UploadTask task = ref.putFile(file);
 
-      // Upload the file to firebase storage
-      UploadTask task = ref.putFile(video);
+  //   // Wait for the task to complete and get the download url
+  //   String url = await (await task).ref.getDownloadURL();
 
-      // Wait for the task to complete and get the download url
-      String url = await (await task).ref.getDownloadURL();
+  //   // Print the url for debugging
+  //   print("@@ ${url}");
+  // }
 
-      // Print the url for debugging
-      print(url);
-    }
-  }
-
-  // Override the build method to return a Dialog widget
   @override
   Widget build(BuildContext context) {
+    List<Widget> attachments = [];
+
+    for (int i = 0; i < files.length; i++) {
+      attachments.add(Icon(
+        Icons.file_present,
+        size: 20,
+      ));
+    }
+
     // Return a Dialog widget with a custom shape and content
     return Dialog(
       // backgroundColor: Colors.transparent,
@@ -587,7 +717,6 @@ class PostDialog extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 10),
-                // A TextField widget for the paragraph input
                 SingleChildScrollView(
                   child: Container(
                     color: Colors.white,
@@ -604,20 +733,41 @@ class PostDialog extends StatelessWidget {
                     ),
                   ),
                 ),
-                // A Row widget for the icon buttons
+                SizedBox(height: 5),
+                files.length > 0
+                    ? SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: attachments,
+                        ),
+                      )
+                    : SizedBox(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     IconButton(
                       icon: Icon(Icons.camera_alt, color: Colors.amber),
-                      onPressed: () {
-                        _pickImage();
+                      onPressed: () async {
+                        var image = await _pickImage();
+                        setState(() {
+                          files.add(image);
+                        });
                       },
                     ),
                     IconButton(
                       icon: Icon(Icons.video_collection_rounded,
                           color: Colors.amber),
-                      onPressed: () {},
+                      onPressed: () async {
+                        File? video = await _pickVideo();
+
+                        if (video != null) {
+                          // Upload the video to firebase
+                          // await uploadFile(video);
+                          setState(() {
+                            files.add(video);
+                          });
+                        }
+                      },
                     ),
                     IconButton(
                       icon: Icon(Icons.link, color: Colors.amber),
@@ -635,3 +785,148 @@ class PostDialog extends StatelessWidget {
     );
   }
 }
+
+// // Define a custom dialog class that extends StatelessWidget
+// class PostDialoga extends StatelessWidget {
+//   // Declare the text editing controllers for the title and the paragraph
+//   final TextEditingController titleController = TextEditingController();
+//   final TextEditingController paragraphController = TextEditingController();
+//   List<dynamic> files = [];
+
+// // Define a function to pick an image from the gallery and return it as a File
+//   Future<File?> _pickImage() async {
+//     // Use the image_picker package to get the image
+//     final pickedFile =
+//         await ImagePicker().pickImage(source: ImageSource.gallery);
+
+//     // Return the image as a File if it is not null, otherwise return null
+//     return pickedFile != null ? File(pickedFile.path) : null;
+//   }
+
+//   Future<File?> _pickVideo() async {
+//     // Use the image_picker package to get the video
+//     final pickedFile =
+//         await ImagePicker().pickVideo(source: ImageSource.gallery);
+
+//     // Return the video as a File if it is not null, otherwise return null
+//     return pickedFile != null ? File(pickedFile.path) : null;
+//   }
+
+//   Future<void> _uploadVideo(File? video, String name) async {
+//     // Check if the video is not null
+//     if (video != null) {
+//       // Create a reference to the firebase storage
+//       FirebaseStorage storage = FirebaseStorage.instance;
+
+//       // Create a reference to the specific location with the given name
+//       Reference ref = storage.ref().child(name);
+
+//       // Upload the file to firebase storage
+//       UploadTask task = ref.putFile(video);
+
+//       // Wait for the task to complete and get the download url
+//       String url = await (await task).ref.getDownloadURL();
+
+//       // Print the url for debugging
+//       print(url);
+//     }
+//   }
+
+//   // Override the build method to return a Dialog widget
+//   @override
+//   Widget build(BuildContext context) {
+//     List<Widget> attachments = [];
+
+//     for (int i = 0; i < files.length; i++) {
+//       attachments.add(Icon(
+//         Icons.file_present,
+//         size: 40,
+//       ));
+//     }
+
+//     // Return a Dialog widget with a custom shape and content
+//     return Dialog(
+//       // backgroundColor: Colors.transparent,
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(12.0),
+//       ),
+//       child: Container(
+//           height: MediaQuery.sizeOf(context).height * 0.6,
+//           width: MediaQuery.sizeOf(context).width,
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(10),
+//             color: Colors.white,
+//           ),
+//           padding: EdgeInsets.only(top: 5.0, left: 10, right: 10, bottom: 5.0),
+//           child: SingleChildScrollView(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.start,
+//               children: <Widget>[
+//                 SizedBox(height: 10),
+//                 Container(
+//                   color: Colors.white,
+//                   height: MediaQuery.sizeOf(context).height * 0.05,
+//                   child: TextField(
+//                     controller: titleController,
+//                     decoration: InputDecoration(
+//                       labelText: 'Title',
+//                       border: OutlineInputBorder(),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 10),
+//                 // A TextField widget for the paragraph input
+//                 SingleChildScrollView(
+//                   child: Container(
+//                     color: Colors.white,
+//                     height: MediaQuery.sizeOf(context).height > 800
+//                         ? MediaQuery.sizeOf(context).height * 0.45
+//                         : MediaQuery.sizeOf(context).height * 0.35,
+//                     child: TextField(
+//                       controller: paragraphController,
+//                       maxLines: 100,
+//                       decoration: InputDecoration(
+//                         labelText: 'Paragraph',
+//                         border: OutlineInputBorder(),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 files.length > 0
+//                     ? SingleChildScrollView(
+//                         scrollDirection: Axis.horizontal,
+//                         child: Row(
+//                           children: attachments,
+//                         ),
+//                       )
+//                     : SizedBox(),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: <Widget>[
+//                     IconButton(
+//                       icon: Icon(Icons.camera_alt, color: Colors.amber),
+//                       onPressed: () {
+//                         var image = _pickImage();
+//                       },
+//                     ),
+//                     IconButton(
+//                       icon: Icon(Icons.video_collection_rounded,
+//                           color: Colors.amber),
+//                       onPressed: () {},
+//                     ),
+//                     IconButton(
+//                       icon: Icon(Icons.link, color: Colors.amber),
+//                       onPressed: () {},
+//                     ),
+//                     IconButton(
+//                       icon: Icon(Icons.send, color: Colors.amber),
+//                       onPressed: () {},
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           )),
+//     );
+//   }
+// }
